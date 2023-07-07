9df5c456114d5b97e54eee0e08acc4dc7febc3a1
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-07-07 13:08:57 +0600

    useMemo added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import UseCallbackComp from './components/useCallbackComp/UseCallbackComp'
 import UseRefComp from './components/useRefComp/UseRefComp'
+import UseMemoComp from './components/useMemoComp/useMemoComp'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,8 @@ function App() {
     <>
       <h1>Learning Hooks</h1>
       {/* <UseCallbackComp></UseCallbackComp> */}
-      <UseRefComp></UseRefComp>
+      {/* <UseRefComp></UseRefComp> */}
+      <UseMemoComp></UseMemoComp>
     </>
   )
 }
